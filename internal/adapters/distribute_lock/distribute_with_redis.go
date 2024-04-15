baf5abf522141dcd18a5b886d20718d6b95592f7
Banh Quoc Danh <banhquocdanh.2011@gmail.com>
2024-04-15 18:40:04 +0700

    feat: new distribute lock
     + create deposit transaction
     + process deposit transaciton

--- internal/adapters/distribute_lock/distribute_with_redis.go
+++ internal/adapters/distribute_lock/distribute_with_redis.go
@@ -58,3 +58,54 @@ func (d DistributeLockWithRedis) AcquireLockForCreateAccountByUserID(ctx context
 	}
 	return releaseLock, nil
 }
+
+func (d DistributeLockWithRedis) AcquireLockForCreateDepositTransaction(ctx context.Context, requestID string, lockDuration time.Duration) (func(), error) {
+	key := fmt.Sprintf("money-transfer.lock.transaction.deposit.create.%s", requestID)
+	mutex := d.rsClient.NewMutex(
+		key,
+		redsync.WithExpiry(lockDuration),
+		redsync.WithTries(d.cfg.LockRetries),
+		redsync.WithRetryDelay(d.cfg.LockRetryDelay),
+	)
+
+	releaseLock := func() {
+		l := logger.FromContext(ctx)
+		ok, err := mutex.Unlock()
+		if !ok || err != nil {
+			l.Errorw("failed to unlock redis lock", "key", key, "error", err, "ok", ok)
+			return // ignore error
+		}
+		l.Infow("unlock redis lock", "key", key)
+	}
+
+	if err := mutex.Lock(); err != nil {
+		return nil, fmt.Errorf("failed to acquire redis lock with key %s: %w", key, err)
+	}
+	return releaseLock, nil
+}
+
+func (d DistributeLockWithRedis) AcquireLockForProcessDepositTransaction(ctx context.Context, transactionID int64, lockDuration time.Duration) (releaseLock func(), err error) {
+	key := fmt.Sprintf("money-transfer.lock.transaction.deposit.process.%d", transactionID)
+	mutex := d.rsClient.NewMutex(
+		key,
+		redsync.WithExpiry(lockDuration),
+		redsync.WithTries(d.cfg.LockRetries),
+		redsync.WithRetryDelay(d.cfg.LockRetryDelay),
+	)
+
+	releaseLock = func() {
+		l := logger.FromContext(ctx)
+		ok, err := mutex.Unlock()
+		if !ok || err != nil {
+			l.Errorw("failed to unlock redis lock", "key", key, "error", err, "ok", ok)
+			return // ignore error
+		}
+		l.Infow("unlock redis lock", "key", key)
+	}
+
+	if err = mutex.Lock(); err != nil {
+		err = fmt.Errorf("failed to acquire redis lock with key %s: %w", key, err)
+		return
+	}
+	return
+}
