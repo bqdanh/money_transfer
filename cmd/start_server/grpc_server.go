f97ad4b8d6f79764c4c7a22e400949bd7ef79298
Banh Quoc Danh <banhquocdanh.2011@gmail.com>
2024-05-05 17:40:55 +0700

    chore: restructure NewGrpcServices func

--- cmd/start_server/grpc_server.go
+++ cmd/start_server/grpc_server.go
@@ -18,7 +18,26 @@ import (
 )
 
 func NewGrpcServices(cfg server.Config, infra *InfrastructureDependencies, adapters *Adapters) ([]grpcadapter.Service, error) {
-	// user application
+	userService, err := NewUserService(cfg, infra, adapters)
+	if err != nil {
+		return nil, fmt.Errorf("failed to new user service: %w", err)
+	}
+	// new account service
+	accountService, err := NewAccountService(cfg, infra, adapters)
+	if err != nil {
+		return nil, fmt.Errorf("failed to new account service: %w", err)
+	}
+	// new transaction service
+	transactionService, err := NewTransactionService(cfg, infra, adapters)
+
+	return []grpcadapter.Service{
+		userService,
+		accountService,
+		transactionService,
+	}, nil
+}
+
+func NewUserService(_ server.Config, _ *InfrastructureDependencies, adapters *Adapters) (*users.UserService, error) {
 	createUserHandler, err := create_user.NewCreateUser(adapters.UserMysqlRepository)
 	if err != nil {
 		return nil, fmt.Errorf("failed to new create user application: %w", err)
@@ -33,29 +52,26 @@ func NewGrpcServices(cfg server.Config, infra *InfrastructureDependencies, adapt
 		CreateUserHandler: createUserHandler,
 		Login:             loginHandler,
 	}
-	// new server
-	// new account service
 	userService := users.NewUserService(userApplications)
+	return userService, nil
+}
 
-	//account application
+func NewAccountService(cfg server.Config, _ *InfrastructureDependencies, adapters *Adapters) (*accounts.AccountService, error) {
 	accountApplication := accounts.AccountApplications{
 		LinkAccount: link_account.NewLinkBankAccount(cfg.LinkAccount, adapters.AccountMysqlRepository, adapters.DistributeLockWithRedis),
 	}
 	// new account service
 	accountService := accounts.NewAccountService(accountApplication)
+	return accountService, nil
+}
 
-	// new transaction application
+func NewTransactionService(_ server.Config, _ *InfrastructureDependencies, adapters *Adapters) (*transactions.TransactionService, error) {
 	transactionsApp := transactions.TransactionApplications{
 		MakeTransactionSync: make_transaction.MakeTransactionSync{},
 	}
 	// mew transaction service
 	transactionService := transactions.NewTransactionService(transactionsApp)
-
-	return []grpcadapter.Service{
-		userService,
-		accountService,
-		transactionService,
-	}, nil
+	return transactionService, nil
 }
 
 func NewAuthenticateGrpcInterceptors(_ *server.Config, _ *InfrastructureDependencies, adapters *Adapters) (grpc.UnaryServerInterceptor, error) {
